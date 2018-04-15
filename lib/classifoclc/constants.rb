561490953d14f173955f50a672ef62ac18c0a639
Sean Redmond <github-smr@sneakemail.com>
2018-04-15 16:17:25 -0400

    Remove some constants
    Remove constants for parameters that aren't unique (or close)
    identifiers. Without pagination, there is no good way to handle large results
    sets without timing out.

--- lib/classifoclc/constants.rb
+++ lib/classifoclc/constants.rb
@@ -1,29 +1,36 @@
 module Classifoclc
 
   URL = "http://classify.oclc.org/classify2/Classify?%s"
+
+  # See http://classify.oclc.org/classify2/api_docs/classify.html
   
+  # API also allows stdnbr, ident, heading, author, and title but without
+  # pagination, the number of results may be too unwieldy
   module Id
-    STDNBR  = :stdnbr
     OCLC    = :oclc
     ISBN    = :isbn
     LCCN    = :lccn
     ISSN    = :issn
     UPC     = :upc
-    IDENT   = :ident
-    HEADING = :heading
     OWI     = :owi
-    AUTHOR  = :author
-    TITLE   = :title
   end
 
   module OrderBy
+    # number of editions
     EDITIONS  = :mancount
+    # holdings
     HOLDINGS  = :hold
+    # date of first edition
     FIRSTYEAR = :lyr
+    # date of latest edition
     LASTYEAR  = :hyr
+    # language
     LANGUAGE  = :ln
+    # FAST subject heading
     HEADING   = :sheading
+    # number of works with this FAST subject heading
     WORKS     = :works
+    # FAST subject type
     SUBJTYPE  = :type
   end
 
